e5cf3b01eebcc9d3c216a2f6e465c4df752675ae
Nataliia <nvasinaa@gmail.com>
2022-07-04 21:14:46 +0300

    Задача 28

---
+++ 002/Program.cs
@@ -0,0 +1,21 @@
+﻿//Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+//4 -> 24 
+//5 -> 120
+
+void Zadacha28()
+        {
+            Console.WriteLine("Введите число:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(FindNamber(number));
+        }
+
+        int FindNamber(int number)
+        {
+            int sum = 1;
+            for (int i = 1; i <= number; i++)
+            {
+                sum = sum * i; 
+            }
+             return sum;
+        }
+Zadacha28();

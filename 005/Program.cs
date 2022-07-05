74de5e0aa997d0de9b7d0bb645fd29570ee2371e
Nataliia <nvasinaa@gmail.com>
2022-07-05 16:14:14 +0300

    Задача 27

---
+++ 005/Program.cs
@@ -0,0 +1,15 @@
+﻿void Zadacha27()
+        {
+            Console.WriteLine("Введите число: ");
+            int numberA = Convert.ToInt32(Console.ReadLine());
+
+            int i = 0;
+            while (numberA > 0)
+            {
+                i = i + numberA%10;
+                numberA = numberA/10;
+                i++;
+            }
+            Console.WriteLine(i);
+        }
+         Zadacha27(); 

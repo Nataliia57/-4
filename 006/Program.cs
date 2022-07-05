9617d0956abdff4152637d0275833dd0493041d4
Nataliia <nvasinaa@gmail.com>
2022-07-05 16:14:40 +0300

    Задача 25

---
+++ 006/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число A: ");
+double A = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+            
+
+void Zadacha25()
+{
+     double S = 1;
+     for (int i = 0; i < B; i++)
+     {
+         S *= A;   
+     }
+     Console.WriteLine(S);
+ }
+ Zadacha25();
